77d351463c0c30bb5b8e9b9ada752a2ed839db0c
thedevaman <devaman2508@gmail.com>
2026-01-21 08:15:14 +0530

    node modules

---
+++ modules.js
@@ -0,0 +1,9 @@
+// const fs = require('fs');
+// const os = require('os');
+// // fs.writeFileSync('test.txt',"trying with modules")
+// console.log(os.platform())
+// console.log(os.hostname())
+// console.log(os.cpus())
+
+const {log} = require('console')   //globeal object can be customized
+log("custome console Log")
