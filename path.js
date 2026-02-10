5939e3af46dd5fce9ad2e291128c033a0cf9ef62
thedevaman <devaman2508@gmail.com>
2026-02-10 09:15:50 +0530

    path module && global constant

---
+++ path.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const file = "text/peter.txt"
+// console.log(path.extname(file))  //extension name
+// console.log(path.dirname(file)) //directry name
+// console.log(path.basename(file)) //basefile name
+
+// console.log(path.resolve("text","peter.txt")); //file location path
+
+console.log(__dirname);
+console.log(__filename)
